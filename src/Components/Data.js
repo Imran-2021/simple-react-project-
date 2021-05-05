34d2819e8b6ae271dea0ad3f33493497d7f0d200
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-05-05 18:32:54 +0600

    this is done services

---
+++ src/Components/Data.js
@@ -0,0 +1,37 @@
+const Sdata =[
+    {
+        image:"https://images.unsplash.com/photo-1492515114975-b062d1a270ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHdlYnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        title:"Web development"
+    }
+,
+    {
+        image:"https://images.unsplash.com/photo-1492515114975-b062d1a270ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHdlYnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        title:"Web development"
+    }
+,
+    {
+        image:"https://images.unsplash.com/photo-1492515114975-b062d1a270ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHdlYnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        title:"Web development"
+    }
+,
+    {
+        image:"https://images.unsplash.com/photo-1492515114975-b062d1a270ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHdlYnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        title:"Web development"
+    }
+,
+    {
+        image:"https://images.unsplash.com/photo-1492515114975-b062d1a270ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHdlYnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        title:"Web development"
+    }
+,
+    {
+        image:"https://images.unsplash.com/photo-1492515114975-b062d1a270ae?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHdlYnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        title:"Web development"
+    }
+]
+
+
+
+
+
+export default Sdata;
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import Card from './Card';
+import Sdata from "./Data"
 
 const Services = () => {
     return (
-        <>
-        <div className="my-5">
-            <h1 className="text-center">Our Services</h1>
+<>
+<div className="my-5">
+    <h1 className="text-center">Our Services</h1>
+    <div className="container-fluid mb-5">
+        <div className="row">
+            <div className="col-10 mx-auto">
+            <div className="row gy-4">
+                    
+            {
+                Sdata.map((data,ind)=>{
+                    return <Card img={data.image}
+                    title={data.title}
+                    />
+                }) 
+            }
 
-
+            </div>  
+            </div>
         </div>
-        </>
+    </div>
+</div>
+</>
     );
 };
 
